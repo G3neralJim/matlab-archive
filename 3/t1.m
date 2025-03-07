d39ec10aacaec4f66b851cef50429dedb2adc52f
G3neralJim <dimit.peis@gmail.com>
2025-03-07 13:04:23 +0200

    update of 3 through terminal

---
+++ 3/t1.m
@@ -0,0 +1,31 @@
+clc;
+clear;
+
+##Size of Matrix
+n = 5;
+
+##Matrix Init
+A = gallery('tridiag', n);
+A0 = A;
+##Number of Eigenvalues
+Neig = 5;
+
+##Inputs
+tol = 1e-6;
+Nmax = 5000;
+
+##Matrices init
+lamdas = zeros(Neig, 1);
+V = zeros(n,Neig);
+
+[v,lamda] = powit(A,Nmax,tol);
+
+##Method
+
+for i = 1:Neig
+##  Power Iteration
+  [V(:,i),lamdas(i)] = powit(A,Nmax,tol);
+##  Weilandt Deflation
+  A=A-lamdas(i)*V(:,i)*V(:,i)';
+end
+
---
+++ 3/t2.m
@@ -0,0 +1,34 @@
+clc;
+clear;
+
+##Size of Matrix
+n = 100;
+
+##Matrix Init
+A = gallery('tridiag', n);
+A0 = A;
+##Number of Eigenvalues
+Neig = 5;
+
+##Inputs
+tol = 1e-6;
+Nmax = 5000;
+
+##Matrices init
+lamdas = zeros(Neig, 1);
+V = zeros(n,Neig);
+
+[v,lamda] = powit(A,Nmax,tol);
+
+##Method
+
+for i = 1:Neig
+##  Power Iteration (should be inverse)
+  [V(i:end,i),lamdas(i)] = powit(A,Nmax,tol);
+##  Weilandt Deflation
+  x = A(1,:)/V(i,i);
+  A=A-V(i:end,i)*x;
+
+  A = A(2:end, 2:end);
+end
+
