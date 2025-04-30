496f34d5a250dd458427f9326c66cc6bcf2f0eb2
G3neralJim <dimit.peis@gmail.com>
2025-04-30 19:19:09 +0300

    kentrikes kai theta

--- 9/delta.m
+++ 9/delta.m
@@ -4,6 +4,7 @@ clear;
 % Parameters
 c = 0.1;
 L = 1;
+theta = 1/2;
 
 nx = 1000;
 nt = 100;
@@ -14,7 +15,7 @@ Tmax = 5*0.42;
 h = L/nx;
 dt = Tmax / nt;
 
-A = -gallery('tridiag', nx-1) / h^2;
+D = -gallery('tridiag', nx-1) / h^2;
 I = speye(nx-1);
 
 % Initial condition
@@ -26,7 +27,10 @@ T(:,1) = sin(pi*x);
 
 % Advance in time
 for i = 2:nt
-    T(2:nx, i) = (I-dt*c*A)\T(2:nx,i-1);
+    T(2:nx, i) = (I - theta*c*dt*D)\(I+(1-theta)*c*dt*D)*T(2:nx,i-1);
 end
 
 mesh(T);
+
+
+
