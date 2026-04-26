22673468cc7b55d11c66519c1a7f135ee29c3c03
G3neralJim <dimit.peis@gmail.com>
2026-04-26 15:15:36 +0300

    progress on decomposition script
    decomposition works for overlap = 1. Got confused on what exactly changes for greater values

--- Advanced_Scientific_Computations/6/deComp1D.m
+++ Advanced_Scientific_Computations/6/deComp1D.m
@@ -10,46 +10,70 @@ h = 1/(n-1);
 % Parameters
 Nmax = 500;
 tol = (h^2)*0.1;
-overlap = 1;
+
+overlap = 1; % must be odd
+op_old = (overlap+1)/2; % initial development wasd done with this variable named as overlap
+                        % but it doesn't accuretly express the overlap value leading to issues
+                        % in generalization. The variable above correctly reflects real overlap
+                        % and I am too lazy to adjust the code, for the time being  
 
 % Number of Points per domain
-nd = (n-1)/2 + overlap;
+nd = (n-1)/2 + op_old;
 
 % Coefficient Matrix
-A = -gallery("tridiag",nd);
+A = -gallery("tridiag",nd-1);
 
 % RHS Function
 f = @(x) - (pi^2)*sin(pi*x);
 
 % Grid positions
-x1 = (h:h:(((n-1)/2+overlap-1)*h))';
-x2 = ((((n-1)/2-overlap+1)*h):h:1-h)';
+x1 = (h:h:(((n-1)/2+op_old-1)*h))';
+x2 = ((((n-1)/2-op_old+1)*h):h:1-h)';
 
-% % Initial RHS
-% b1 = h^2*f(x1);
-% b2 = h^2*f(x2);
+% Initial RHS
+b1 = h^2*f(x1);
+b2 = h^2*f(x2);
 
-% % Initial Solutions
-% u1 = A\b1;
-% u2 = A\b2;
+% Initial Solutions
+u1 = A\b1;
+u2 = A\b2;
+uc = u2(op_old);
 
+b1t = b1;
+b2t = b2;
 
-% for iter = 1:Nmax
-%     plot(x1,u1);
-%     hold;
-%     plot(x2,u2);
-%     hold off;
+for iter = 1:Nmax
+    plot(x1,u1);
+    hold;
+    plot(x2,u2);
+    hold off;
 
-%     b1t = b1;
-%     b2t = b2;
+    [u1 u2]
+    % aa = input('');
 
-%     b1t(end) = b1t(end)-u2(2);
-%     b2t(1) = b2t(1) - u1(end-1);
+    b1t = b1;
+    b2t = b2;
 
-%     u1 = A\b1t;
-%     u2 = A\b2t;
-% end
+    b1t((end-overlap)+1:end) = b1t((end-overlap)+1:end)-u2(2);
+    b2t(1:(overlap-1)) = b2t(1:(overlap-1))-u1(end-1);
 
+    ucp = u2(op_old);
+    u1 = A\b1t;
+    u2 = A\b2t;
+    uc = u2(op_old);
+
+    if norm(ucp-uc)<tol
+        break;
+    end
+
+end
+
+u = [u1(1:end-op_old); u2];
+xx = h:h:((n-2)*h)';
+u_ = @(x) sin(pi*x);
+
+error = abs(u_(xx)'-u);
+max(error)';
 
 
 
